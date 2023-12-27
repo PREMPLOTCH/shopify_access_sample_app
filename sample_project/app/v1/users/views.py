8f8f5d178ab6a3ec34ab978b1275cacc57c55a05
PREMBK <150332466+PREMPLOTCH@users.noreply.github.com>
2023-12-27 11:35:30 +0530

    Update views.py

--- sample_project/app/v1/users/views.py
+++ sample_project/app/v1/users/views.py
@@ -1,18 +1,18 @@
 from flask.views import MethodView
 from app.decorators import validate_request
-from app.v1.users.service import SubApp1Service
+from app.v1.users.service import ShopifyAccessTokenService
 from flask import request
 
 class AppIndex(MethodView):
     @validate_request
     def get(self, params, headers, *args, **kwargs):
-        response = SubApp1Service(params, headers).app_index()
+        response = ShopifyAccessTokenService(params, headers).app_index()
         return response
 
 class InstallApp(MethodView):  
     @validate_request
     def get(self, params, headers, *args, **kwargs):
-        response = SubApp1Service(params, headers).install_app()
+        response = ShopifyAccessTokenService(params, headers).install_app()
         return response    
 
 class AuthorizeApp(MethodView):
@@ -20,5 +20,5 @@ class AuthorizeApp(MethodView):
     def get(self, params, headers, *args, **kwargs):
         code = request.args.get('code')
         shop = request.args.get('shop')
-        response = SubApp1Service(params, headers).authorize_app_after_install()
+        response = ShopifyAccessTokenService(params, headers).authorize_app_after_install()
         return response
