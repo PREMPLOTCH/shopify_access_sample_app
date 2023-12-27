a57a9e3b13f67e5ded639b8e415cb4e265fbe9df
PREMBK <150332466+PREMPLOTCH@users.noreply.github.com>
2023-12-27 11:33:50 +0530

    Update views.py

--- sample_project/app/v1/users/views.py
+++ sample_project/app/v1/users/views.py
@@ -4,21 +4,18 @@ from app.v1.users.service import SubApp1Service
 from flask import request
 
 class AppIndex(MethodView):
-
     @validate_request
     def get(self, params, headers, *args, **kwargs):
         response = SubApp1Service(params, headers).app_index()
         return response
 
-class InstallApp(MethodView):
-    
+class InstallApp(MethodView):  
     @validate_request
     def get(self, params, headers, *args, **kwargs):
         response = SubApp1Service(params, headers).install_app()
         return response    
 
 class AuthorizeApp(MethodView):
-
     @validate_request
     def get(self, params, headers, *args, **kwargs):
         code = request.args.get('code')
